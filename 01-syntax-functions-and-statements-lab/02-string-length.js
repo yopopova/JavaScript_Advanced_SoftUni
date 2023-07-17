fab2d185be8247d9b070027a8b2f95ed8ea92105
Yoanna Popova <yoanna.dp@gmail.com>
2023-07-17 21:01:38 +0300

    Add solution to second task.

---
+++ 01-syntax-functions-and-statements-lab/02-string-length.js
@@ -0,0 +1,13 @@
+function stringLength(arg1, arg2, arg3) {
+    const count = 3;
+
+    let argOne = arg1.length;
+    let argTwo = arg2.length;
+    let argThree = arg3.length;
+
+    let sumL = argOne + argTwo + argThree;
+    let average = Math.floor(sumL / count);
+
+    console.log(sumL);
+    console.log(average);
+}
