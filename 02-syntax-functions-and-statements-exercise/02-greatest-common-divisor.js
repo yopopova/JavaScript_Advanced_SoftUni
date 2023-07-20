bc67d94e5c2177058f2a4dcea8b0e3cc04e3d250
Yoanna Popova <yoanna.dp@gmail.com>
2023-07-20 08:57:56 +0300

    Add solution to second task.

---
+++ 02-syntax-functions-and-statements-exercise/02-greatest-common-divisor.js
@@ -0,0 +1,14 @@
+function greatestCommonDivisor(a, b) {
+    let firstNumber = Number(a);
+    let secondNumber = Number(b);
+
+    while (firstNumber !== secondNumber) {
+        if (firstNumber > secondNumber) {
+            firstNumber -= secondNumber;
+        } else {
+            secondNumber -= firstNumber;
+        }
+    }
+
+    console.log(firstNumber);
+}
