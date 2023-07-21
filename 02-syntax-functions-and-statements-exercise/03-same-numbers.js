1c4247af0e5fc7b97204697d65d651494623d706
Yoanna Popova <yoanna.dp@gmail.com>
2023-07-21 09:47:03 +0300

    Add solution to third task.

---
+++ 02-syntax-functions-and-statements-exercise/03-same-numbers.js
@@ -0,0 +1,21 @@
+function sameNumbers(input) {
+    let inputNumber = input.toString();
+    let inputNumberL = inputNumber.length;
+
+    let digitToCompare = Number(inputNumber[0]);
+
+    let isSame = true;
+    let sumOfDigits = 0;
+
+    for (let i = 0; i < inputNumberL; i++) {
+        let currentDigit = Number(inputNumber[i]);
+        sumOfDigits += currentDigit;
+
+        if (currentDigit !== digitToCompare) {
+            isSame = false;
+        }
+    }
+
+    console.log(isSame);
+    console.log(sumOfDigits);
+}
