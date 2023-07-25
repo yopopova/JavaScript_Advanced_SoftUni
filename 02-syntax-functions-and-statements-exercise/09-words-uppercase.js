8d911e7960e8cb810b956695c8ae0a95d2f8bc86
Yoanna Popova <yoanna.dp@gmail.com>
2023-07-25 09:50:24 +0300

    Add solution to ninth task.

---
+++ 02-syntax-functions-and-statements-exercise/09-words-uppercase.js
@@ -0,0 +1,13 @@
+function wordsUppercase(str) {
+    let regEx = /[a-zA-z]+/g;
+    let validMatches = str.match(regEx);
+
+    let arrUpperCase = [];
+
+    for (el of validMatches) {
+        let upperLetters = el.toUpperCase();
+        arrUpperCase.push(upperLetters);
+    }
+
+    console.log(arrUpperCase.join(', '));
+}
