7d847350e2ae56e52b590d37a3fea1e47c5aff39
Yoanna Popova <yoanna.dp@gmail.com>
2023-07-28 08:56:01 +0300

    Add solution to eighth task.

---
+++ 03-arrays-and-nested-arrays-lab/08-process-odd-positions.js
@@ -0,0 +1,16 @@
+function processOddPositions(arr) {
+    let arrL = arr.length;
+    let newArr = [];
+
+    for (let i = 0; i < arrL; i++) {
+        let currentNum = arr[i];
+
+        if (i % 2 === 1) {
+            let doubledNum = currentNum * 2;
+            newArr.push(doubledNum);
+        }
+    }
+
+    let reversedArr = newArr.reverse();
+    return reversedArr.join(' ');
+}
