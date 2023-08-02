fd8ef18b2fdd38298bc3b205d074b70baf0c7206
Yoanna Popova <yoanna.dp@gmail.com>
2023-08-02 08:15:00 +0300

    Add second solution to sixth task.

---
+++ 04-arrays-and-nested-arrays-exercise/06-list-of-names-v2.js
@@ -0,0 +1,12 @@
+function listOfNames(arr) {
+    let sortedArr = arr.sort((a, b) => {
+        return a.localeCompare(b);
+    });
+
+    let orderNum = 1;
+
+    sortedArr.forEach((name) => {
+        console.log(`${orderNum}.${name}`);
+        orderNum++;
+    });
+}
