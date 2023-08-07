97ea39da22cad786f96abe0e5c8b5ee8a8d50cfb
Yoanna Popova <yoanna.dp@gmail.com>
2023-08-07 18:05:43 +0300

    Add solution to first task.

---
+++ 05-objects-and-composition-lab/01-city-record.js
@@ -0,0 +1,9 @@
+function cityRecord(city, population, treasury) {
+    let record = {
+        name: city,
+        population: population,
+        treasury: treasury,
+    };
+
+    return record;
+}
