f17e39df3de71b3686d3b2c5b9f1f023b9195753
Yoanna Popova <yoanna.dp@gmail.com>
2023-08-17 08:31:29 +0300

    Add solution to fourth task.

---
+++ 07-DOM-introduction-lab/04-show-more/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let textElement = document.getElementById('text');
+    textElement.style.display = 'inline';
+
+    let hidenElement = document.getElementById('more');
+    hidenElement.style.display = 'none';
+}
