a99cd39e7354557682fa8d9b2786bfafd2126d22
Yoanna Popova <yoanna.dp@gmail.com>
2023-08-18 09:10:59 +0300

    Add solution to fifth task.

---
+++ 07-DOM-introduction-lab/05-colorize-table/ColorizeTable.js
@@ -0,0 +1,10 @@
+function colorize() {
+    let rowElements = document.getElementsByTagName('tr');
+    let rows = Array.from(rowElements);
+
+    rows.forEach((x, i) => {
+        if (i % 2 !== 0) {
+            x.style.backgroundColor = 'teal';
+        }
+    });
+}
