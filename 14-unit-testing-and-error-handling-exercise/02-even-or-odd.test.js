317b2eddbd7cb2fa7f1a68db5951f488b5b2592f
Yoanna Popova <yoanna.dp@gmail.com>
2023-09-08 09:04:07 +0300

    Add test file to second task.

---
+++ 14-unit-testing-and-error-handling-exercise/02-even-or-odd.test.js
@@ -0,0 +1,45 @@
+const isOddOrEven = require('./02-even-or-odd.js');
+const { assert } = require('chai');
+
+// В Judge предаваме САМО 'describe'!!!
+
+describe('isOddOrEven function tests', () => {
+
+    // Invalid input tests
+    it('Should return undefined if the parameter is number.', () => {
+        assert.equal(isOddOrEven(10), undefined);
+    });
+
+    it('Should return undefined if the parameter is object.', () => {
+        assert.equal(isOddOrEven({}), undefined);
+    });
+
+    it('Should return undefined if the parameter is array.', () => {
+        assert.equal(isOddOrEven([]), undefined);
+    });
+
+    it('Should return undefined if the parameter is undefined.', () => {
+        assert.equal(isOddOrEven(undefined), undefined);
+    });
+
+    it('Should return undefined if the parameter is null.', () => {
+        assert.equal(isOddOrEven(null), undefined);
+    });
+
+
+    // Valid input tests
+    it('Should return even as result.', () => {
+        assert.equal(isOddOrEven('Hi'), 'even');
+    });
+
+    it('Should return odd as result.', () => {
+        assert.equal(isOddOrEven('Hello'), 'odd');
+    });
+
+
+    // Broken test
+    // it('Should return odd as result.', () => {
+    //     assert.equal(isOddOrEven('Hell'), 'odd');
+    // });
+
+});
