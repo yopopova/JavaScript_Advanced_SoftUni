9841d0908cc4e1a7c38539808ae5f033ad8e4182
Yoanna Popova <yoanna.dp@gmail.com>
2023-09-12 16:25:50 +0300

    Add solution to second task.

---
+++ 15-classes-lab/02-get-persons.js
@@ -0,0 +1,21 @@
+function getPersons() {
+    class Person {
+        constructor(firstName, lastName, age, email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+        }
+    }
+
+    let arrPeople = [];
+    arrPeople.push(new Person('Anna', 'Simpson', 22, 'anna@yahoo.com'));
+    arrPeople.push(new Person('SoftUni'));
+    arrPeople.push(new Person('Stephan', 'Johnson', 25));
+    arrPeople.push(new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com'));
+    return arrPeople;
+}
