419bc2e69000b11978dc76c4e17c654654f47935
Yoanna Popova <yoanna.dp@gmail.com>
2023-09-13 09:59:42 +0300

    Add solution to fourth task.

---
+++ 16-classes-exercise/04-sorted-list.js
@@ -0,0 +1,29 @@
+class List {
+    constructor() {
+        this.result = [];
+        this.size = this.result.length;
+    }
+
+    add(value) {
+        this.result.push(value);
+        this.size = this.result.length;
+        this.result.sort((a, b) => a - b);
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.result.length) {
+            throw new Error('Invalid index!');
+        }
+
+        this.result.splice(index, 1);
+        this.size = this.result.length;
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.result.length) {
+            throw new Error('Invalid index!');
+        }
+
+        return this.result[index];
+    }
+}
