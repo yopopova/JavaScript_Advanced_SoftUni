2afb1dedaa65ccee2390df368ba8b97f6a9a9e28
Yoanna Popova <yoanna.dp@gmail.com>
2023-09-15 08:29:51 +0300

    Add solution to fifth task.

---
+++ 18-prototypes-and-inheritance-exercise/05-people.js
@@ -0,0 +1,59 @@
+function people() {
+    class Employee {
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+            this.salary = 0;
+            this.tasks = [];
+            this.currentTask = 0;
+        }
+
+        work() {
+            console.log(this.tasks[this.currentTask]);
+            this.currentTask++;
+
+            if (this.tasks.length - 1 < this.currentTask) {
+                this.currentTask = 0;
+            }
+        }
+
+        collectSalary() {
+            console.log(`${this.name} received ${this.salary} this month.`);
+        }
+    }
+
+    class Junior extends Employee {
+        constructor(name, age) {
+            super(name, age);
+            this.tasks = [`${this.name} is working on a simple task.`];
+        }
+    }
+
+    class Senior extends Employee {
+        constructor(name, age) {
+            super(name, age);
+            this.tasks =
+                [`${this.name} is working on a complicated task.`,
+                `${this.name} is taking time off work.`,
+                `${this.name} is supervising junior workers.`];
+        }
+    }
+
+    class Manager extends Employee {
+        constructor(name, age) {
+            super(name, age);
+            this.tasks =
+                [`${this.name} scheduled a meeting.`,
+                `${this.name} is preparing a quarterly report.`];
+            this.dividend = 0;
+        }
+
+        collectSalary() {
+            console.log(`${this.name} received ${this.salary + this.dividend} this month.`);
+        }
+    }
+
+    return {
+        Employee, Junior, Senior, Manager
+    }
+}
